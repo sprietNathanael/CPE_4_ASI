982ade8803757de8670382f8f8c585b5032ba9d5
ThomasDescroix <descroix.thomas@gmail.com>
2018-05-22 15:27:48 +0200

    Add Card

--- WebServiceCard/WebContent/js/form-sample.js
+++ WebServiceCard/WebContent/js/form-sample.js
@@ -1,15 +1,25 @@
-console.log('Hello world');
-
-$("#submit").click(function(e){
+$("#form").submit(function(e){ 
+	var formData = {
+			name: $('[name="name"]').val(),
+			description: $('[name="description"]').val(),
+			family: $('[name="family"]').val(),
+			hp: $('[name="hp"]').val(),
+			energy: $('[name="energy"]').val(),
+			attack: $('[name="attack"]').val(),
+			defence: $('[name="defence"]').val(),
+			imageUrl: $('[name="imageUrl"]').val()
+	}
 	$.ajax({
-		url : '/add',
-		type : 'POST',
-		data: $(this).serialize(),
-		success : function(data){
-			console.log('success');
-		},
-		error : function(resultat, statut, erreur){
-			console.log('error');console.log(erreur);
+		type: "POST",
+		contentType : "application/json",
+		url: "/WebServiceCard/rest/servicescard/add",
+		data: JSON.stringify(formData),
+		dataType: 'json',
+		success: function(e){
+			alert('yes');
+		}, 
+		error : function(e){
+			console.log(e);
 		}
 	});
 });
