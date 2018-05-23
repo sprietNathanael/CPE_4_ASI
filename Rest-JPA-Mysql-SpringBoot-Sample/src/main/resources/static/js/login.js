1ad1e2e789ee65bc4db9d9ab75c13360e88bce8a
sprietNathanael <nathanael.spriet@gmail.com>
2018-05-23 09:28:46 +0200

    Make the login form work

--- Rest-JPA-Mysql-SpringBoot-Sample/src/main/resources/static/js/login.js
+++ Rest-JPA-Mysql-SpringBoot-Sample/src/main/resources/static/js/login.js
@@ -1,6 +1,7 @@
 console.log("login.js load");
 
 $("#submit").click(function(){
+	$("#loginError").addClass("hidden");
 	var formData = {
 			surname: $('[name="surname"]').val(),
 			password: $('[name="password"]').val()
@@ -8,15 +9,21 @@ $("#submit").click(function(){
 	$.ajax({
 		type: "GET",
 		contentType : "application/json",
-		url: "/login",
-		data: JSON.stringify(formData),
-		dataType: 'json',
+		url: "/login?surname="+formData.surname+"&password="+formData.password,
 		success: function(data){
-			console.log(data);
+			if(data)
+			{
+				window.location = "/cardHome.html";
+			}
+			else
+			{
+				$("#loginError").removeClass("hidden");			
+			}
 		}, 
 		error : function(){
 			console.log('Echec');
 		}
 	});
+	return false;
 });
 
