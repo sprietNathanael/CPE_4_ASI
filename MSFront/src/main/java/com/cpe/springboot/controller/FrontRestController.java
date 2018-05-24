4485af4542e7595cab56983652e3811bda1b0766
tp <tp@lxFull>
2018-05-24 14:54:43 +0200

    MSFront Controller refactored

--- MSFront/src/main/java/com/cpe/springboot/controller/FrontRestController.java
+++ MSFront/src/main/java/com/cpe/springboot/controller/FrontRestController.java
@@ -15,36 +15,36 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class FrontRestController {
 	
-	RestTemplate restTemplate = new RestTemplate();
-	String userServer = "http://localhost:8083/";
+	private RestTemplate restTemplate = new RestTemplate();
+	private final String USER_SERVER = "http://localhost:8083/";
 	
 	@RequestMapping(path = "/users", produces = "application/json")
 	private String getAllUsers() {
-		String result = restTemplate.getForObject(userServer+"getall", String.class);
+		String result = restTemplate.getForObject(USER_SERVER + "getall", String.class);
 		return result;
 	}
 	
 	@RequestMapping(path = "/users/{id}", produces = "application/json")
 	private String getUser(@PathVariable String id) {
-		String result = restTemplate.getForObject(userServer+"id/"+id, String.class);
+		String result = restTemplate.getForObject(USER_SERVER+"id/"+id, String.class);
 		return result;
 	}
 	
 	@RequestMapping(path = "/login", produces = "application/json")
 	private String login(String surname, String password) {
-		String result = restTemplate.getForObject(userServer+"login/?surname="+surname+"&password="+password, String.class);
+		String result = restTemplate.getForObject(USER_SERVER +"login/?surname="+surname+"&password="+password, String.class);
 		return result;
 	}
 	
 	@RequestMapping(path = "/logout", produces = "application/json")
 	private String logout(String id) {
-		String result = restTemplate.getForObject(userServer+"logout/?id="+id, String.class);
+		String result = restTemplate.getForObject(USER_SERVER +"logout/?id="+id, String.class);
 		return result;
 	}
 	
 	@RequestMapping(path = "/tryToken", produces = "application/json")
 	private String tryToken(String id, String token) {
-		String result = restTemplate.getForObject(userServer+"tryToken/?id="+id+"&token="+token, String.class);
+		String result = restTemplate.getForObject(USER_SERVER+"tryToken/?id="+id+"&token="+token, String.class);
 		return result;
 	}
 	
@@ -56,7 +56,7 @@ public class FrontRestController {
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		// Data attached to the request.
 		HttpEntity<String> requestBody = new HttpEntity<>(user, headers);
-		restTemplate.postForObject(userServer, requestBody, String.class);
+		restTemplate.postForObject(USER_SERVER, requestBody, String.class);
 		return ResponseEntity.ok().build();
 	}
 	
@@ -69,12 +69,12 @@ public class FrontRestController {
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		// Data attached to the request.
 		HttpEntity<String> requestBody = new HttpEntity<>(user, headers);
-		restTemplate.put(userServer+"id/"+id, requestBody, String.class);
+		restTemplate.put(USER_SERVER+"id/"+id, requestBody, String.class);
 	}
 	
 	@RequestMapping(method=RequestMethod.DELETE,value="/users/{id}")
 	public void deleteUser(@PathVariable String id) {
-		restTemplate.delete(userServer+"id/"+id);
+		restTemplate.delete(USER_SERVER+"id/"+id);
 	}
 
 }
