430739b1b0c8953898ab048dba31c6310e6e0ef1
sprietNathanael <nathanael.spriet@gmail.com>
2018-05-25 16:27:41 +0200

    Fill the cards into the select card page

--- MSFront/src/main/resources/static/js/room/selectCard.js
+++ MSFront/src/main/resources/static/js/room/selectCard.js
@@ -1,16 +1,7 @@
 $(document ).ready(function(){
     
     $('#roomNameId')[0].innerText=" A ";
-    
-    
-    fillCurrentCard("https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/280px-DC_Comics_logo.png","DC comics","http://www.guinnessworldrecords.com/images/superlative/superheroes/GWR-Superheroes-SUPERMAN.svg","SUPERMAN","The origin story of Superman relates that he was born Kal-El on the planet Krypton, before being rocketed to Earth as an infant by his scientist father Jor-El, moments before Krypton's destruction. Discovered and adopted by a farm couple from Kansas, the child is raised as Clark Kent and imbued with a strong moral compass. Early in his childhood, he displays various superhuman abilities, which, upon reaching maturity, he resolves to use for the benefit of humanity through a 'Superman' identity.",50,100,17,8,100);
-
-
-    for(i=0;i<5;i++){
-        addCardToList(i,"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/280px-DC_Comics_logo.png","DC comics","http://www.guinnessworldrecords.com/images/superlative/superheroes/GWR-Superheroes-SUPERMAN.svg","SUPERMAN","The origin story of Superman relates that he was born Kal-El on the planet Krypton, before being rocketed to Earth as an infant by his scientist father Jor-El, moments before Krypton's destruction. Discovered and adopted by a farm couple from Kansas, the child is raised as Clark Kent and imbued with a strong moral compass. Early in his childhood, he displays various superhuman abilities, which, upon reaching maturity, he resolves to use for the benefit of humanity through a 'Superman' identity.",50,100,17,80);
-    }
-    
-    
+        
     $("#playButtonId").click(function(){
         alert("Play button clicked ");
         //TO DO
@@ -20,25 +11,62 @@ $(document ).ready(function(){
 });
 
 
+function authReady()
+{
+	console.log("auth ready");
+	$("#userNameId").text(user.name);
+	$("#userCash").text(user.cash);
+}
+
+var currentCard;
+var cards = [];
 
 
-function fillCurrentCard(imgUrlFamily,familyName,imgUrl,name,description,hp,energy,attack,defence,price){
-    //FILL THE CURRENT CARD
-    $('#cardFamilyImgId')[0].src=imgUrlFamily;
-    $('#cardFamilyNameId')[0].innerText=familyName;
-    $('#cardImgId')[0].src=imgUrl;
-    $('#cardNameId')[0].innerText=name;
-    $('#cardDescriptionId')[0].innerText=description;
-    $('#cardHPId')[0].innerText=hp+" HP";
-    $('#cardEnergyId')[0].innerText=energy+" Energy";
-    $('#cardAttackId')[0].innerText=attack+" Attack";
-    $('#cardDefenceId')[0].innerText=defence+" Defence";
-  
+$(window).on('load', function(){ 
+	$.ajax({
+		type: "GET",
+		contentType : "application/json",
+		url: "/cards/user/" + user.id+ ""+completeURLWithToken(),
+		success: function(data){
+			console.log('OK');
+			console.log(data);
+			$.each(data, function(index, card){
+				addCardToList(index,card.id,"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/280px-DC_Comics_logo.png",card.family,card.imgUrl,card.name,card.description,card.hp,card.energy,card.attack,card.defence,card.price);
+				cards.push(card);
+			});
+			currentCard = 0;
+			fillCurrentCard();
+		}, 
+		error : function(){
+			console.log('Echec');
+		}
+	});
+});
+
+
+function fillCurrentCard(){
+	//FILL THE CURRENT CARD
+    $('#cardFamilyImgId')[0].src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/280px-DC_Comics_logo.png";
+    $('#cardFamilyNameId')[0].innerHTML=cards[currentCard].family;
+    $('#cardImgId')[0].src=cards[currentCard].imgUrl;
+    $('#cardNameId')[0].innerHTML=cards[currentCard].name;
+    $('#cardDescriptionId')[0].innerHTML=cards[currentCard].description;
+    $('#cardHPId')[0].innerHTML=cards[currentCard].hp+" HP";
+    $('#cardEnergyId')[0].innerHTML=cards[currentCard].energy+" Energy";
+    $('#cardAttackId')[0].innerHTML=cards[currentCard].attack+" Attack";
+    $('#cardDefenceId')[0].innerHTML=cards[currentCard].defence+" Defence";
+   
 };
 
+function updateCurrentCard(cardIndex){
+	currentCard = parseInt(cardIndex);
+	fillCurrentCard();
 
-function addCardToList(id,imgUrlFamily,familyName,imgUrl,name,description,hp,energy,attack,defence){
-    
+}
+
+
+function addCardToList(index,idCard,imgUrlFamily,familyName,imgUrl,name,description,hp,energy,attack,defence,price){
+	  
     content="\
     <td> \
     <img  class='ui avatar image' src='"+imgUrl+"'> <span>"+name+" </span> \
@@ -50,19 +78,18 @@ function addCardToList(id,imgUrlFamily,familyName,imgUrl,name,description,hp,ene
     <td>"+attack+"</td> \
     <td>"+defence+"</td> \
     <td>\
-        <div class='ui vertical animated button' tabindex='0' onClick='onCardSelected("+id+")'>\
+        <div onclick=\"updateCurrentCard("+index+")\" class='ui vertical animated button' tabindex='0'>\
             <div class='hidden content'>Select</div>\
     <div class='visible content'>\
         <i class='checkmark icon'></i>\
     </div>\
     </div>\
     </td>";
-    
-    $('#cardListId tr:last').after('<tr>'+content+'</tr>');
+
+    $('#cardList').append('<tr>'+content+'</tr>');
     
     
 };
-
 
 function onCardSelected(id){
     alert("Card selected : " +id);
